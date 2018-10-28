19033d5aef2c82a40cbe1497f46709057334e054
Andrey Sergeev <andrey@chyuck.com>
2018-10-28 15:04:13 -0400

    Added folder for chapter 5 exercise

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ iOS Programming. The Big Nerd Ranch Guide. 6th Edition
 * Quiz (Chapter 1)
 * MyPlayground (Chapter 2)
 * WorldTrotter (Chapter 3,4)
+* WorldTrotter5 (Chapter 5)
---
+++ WorldTrotter5/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celciusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelciusLabel()
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelciusLabel()
+        }
+    }
+    
+    var celciusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelciusLabel() {
+        if let celciusValue = celciusValue {
+            celciusLabel.text = numberFormatter.string(from: NSNumber(value: celciusValue.value))
+        } else {
+            celciusLabel.text = "???"
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        let replacementTextHasAlphabeticCharacter = string.rangeOfCharacter(from: NSCharacterSet.letters)
+        
+        if (existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil) || replacementTextHasAlphabeticCharacter != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+}
