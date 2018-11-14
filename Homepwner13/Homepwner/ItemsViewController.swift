75942ae0bc51bd06719e6f70b4ca290fbab36b8e
Andrey Sergeev <andrey@chyuck.com>
2018-11-13 19:02:37 -0500

    Completed Homepwner for Chapter 13

--- Homepwner13/Homepwner/ItemsViewController.swift
+++ Homepwner13/Homepwner/ItemsViewController.swift
@@ -80,6 +80,19 @@ class ItemsViewController: UITableViewController {
         
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 65
-        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "showItem"?:
+            if let row = tableView.indexPathForSelectedRow?.row {
+                
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destination as! DetailViewController
+                detailViewController.item = item
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
     }
 }
