ea5e5882fd77fa0c16739ae7fb9cece631adad6b
Andrey Sergeev <andrey@chyuck.com>
2018-11-15 22:26:37 -0500

    Completed Homepwner project for Chapter 14

--- Homepwner14/Homepwner/AppDelegate.swift
+++ Homepwner14/Homepwner/AppDelegate.swift
@@ -11,7 +11,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
        
         let itemStore = ItemStore()
         
-        let itemsController = window!.rootViewController as! ItemsViewController
+//        let itemsController = window!.rootViewController as! ItemsViewController
+        let navController = window!.rootViewController as! UINavigationController
+        let itemsController = navController.topViewController as! ItemsViewController;
         itemsController.itemStore = itemStore
         
         return true
--- Homepwner14/Homepwner/ItemsViewController.swift
+++ Homepwner14/Homepwner/ItemsViewController.swift
@@ -2,9 +2,16 @@ import UIKit
 
 class ItemsViewController: UITableViewController {
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+
     var itemStore: ItemStore!
     
-    @IBAction func addNewItem(_ sender: UIButton) {
+//    @IBAction func addNewItem(_ sender: UIButton) {
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
         let newItem = itemStore.createItem()
         
         if let index = itemStore.allItems.index(of: newItem) {
@@ -14,17 +21,17 @@ class ItemsViewController: UITableViewController {
         }
     }
     
-    @IBAction func toggleEditingMode(_ sender: UIButton) {
-        if isEditing {
-            sender.setTitle("Edit", for: .normal)
-            
-            setEditing(false, animated: true)
-        } else {
-            sender.setTitle("Done", for: .normal)
-            
-            setEditing(true, animated: true)
-        }
-    }
+//    @IBAction func toggleEditingMode(_ sender: UIButton) {
+//        if isEditing {
+//            sender.setTitle("Edit", for: .normal)
+//            
+//            setEditing(false, animated: true)
+//        } else {
+//            sender.setTitle("Done", for: .normal)
+//            
+//            setEditing(true, animated: true)
+//        }
+//    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
@@ -72,11 +79,11 @@ class ItemsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        
-        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
-        tableView.contentInset = insets
-        tableView.scrollIndicatorInsets = insets
+//        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+//
+//        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+//        tableView.contentInset = insets
+//        tableView.scrollIndicatorInsets = insets
         
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 65
@@ -94,5 +101,11 @@ class ItemsViewController: UITableViewController {
         default:
             preconditionFailure("Unexpected segue identifier.")
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
     }
 }
