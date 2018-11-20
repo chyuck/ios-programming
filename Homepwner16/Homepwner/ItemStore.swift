f350c3467b3064e4781a95cf65e515d6a34ea1a0
Andrey Sergeev <andrey@chyuck.com>
2018-11-19 23:06:33 -0500

    Completed Homepwner project for Chapter 16

--- Homepwner16/Homepwner/ItemStore.swift
+++ Homepwner16/Homepwner/ItemStore.swift
@@ -3,6 +3,18 @@ import UIKit
 class ItemStore {
     
     var allItems = [Item]()
+    let itemArchiveURL: URL = {
+        let documentsDirectories =
+            FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("items.archive")
+    }()
+    
+    init() {
+        if let archivedItems = NSKeyedUnarchiver.unarchiveObject(withFile: itemArchiveURL.path) as? [Item] {
+            allItems = archivedItems
+        }
+    }
     
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
@@ -28,5 +40,10 @@ class ItemStore {
         allItems.remove(at: fromIndex)
         
         allItems.insert(movedItem, at: toIndex)
+    }
+    
+    func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL.path)")
+        return NSKeyedArchiver.archiveRootObject(allItems, toFile: itemArchiveURL.path)
     }
 }
